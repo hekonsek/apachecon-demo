75d36a4db8385fd699a8514275d1091942e92bd6
Henryk Konsek <hekonsek@gmail.com>
2014-10-30 21:53:04 +0100

    Created endpoint router.

--- apachecon-demo-router/src/main/java/io/fabric8/quickstarts/java/simple/fatjar/Main.java
+++ apachecon-demo-router/src/main/java/io/fabric8/quickstarts/java/simple/fatjar/Main.java
@@ -15,12 +15,37 @@
  */
 package io.fabric8.quickstarts.java.simple.fatjar;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.connection.CachingConnectionFactory;
 
+import javax.jms.ConnectionFactory;
+
+@EnableAutoConfiguration
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         new SpringApplication(Main.class).run(args);
+    }
+
+    @Bean
+    public RoutesBuilder nettyEndpoint() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("netty-http:http://0.0.0.0:18080").
+                        inOnly("jms:invoices").setBody(constant("Received."));
+            }
+        };
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory(new ActiveMQConnectionFactory("tcp://amqbroker:61616"));
     }
 
 }
