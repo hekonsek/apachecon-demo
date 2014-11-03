871bceb78a8961850605041045ce9566c4d310cb
Henryk Konsek <hekonsek@gmail.com>
2014-11-03 11:01:04 +0100

    Added processor route.

---
+++ apachecon-demo-router-processor/src/main/java/io/fabric8/quickstarts/java/simple/fatjar/Main.java
@@ -0,0 +1,51 @@
+/**
+ *  Copyright 2005-2014 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.fabric8.quickstarts.java.simple.fatjar;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.connection.CachingConnectionFactory;
+
+import javax.jms.ConnectionFactory;
+
+@EnableAutoConfiguration
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        new SpringApplication(Main.class).run(args);
+    }
+
+    @Bean
+    public RoutesBuilder nettyEndpoint() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("netty-http:http://0.0.0.0:18080").
+                        inOnly("jms:invoices").setBody(constant("Received."));
+            }
+        };
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory(new ActiveMQConnectionFactory("tcp://amqbroker:6162"));
+    }
+
+}
