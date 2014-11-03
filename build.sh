b89822369618c4953c4ddedd0fbb5ece79032d9c
Henryk Konsek <hekonsek@gmail.com>
2014-11-03 21:46:58 +0100

    Polished script.

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-mvn clean install
-mvn -f apachecon-demo-router/pom.xml install docker:build
-mvn -f apachecon-demo-router-processor/pom.xml install docker:build
-
+# Clean existing Docker processes
 docker stop $(docker ps -q)
 docker rm amqbroker
 docker rm mongodb
 
+# Start MongoDB server & AMQ broker
 docker run -d -p 27017:27017 --name mongodb dockerfile/mongodb
 docker run -d -v /tmp/amqbroker:/var/activemq --name amqbroker fabric8/fabric8-mq:hk.1
+
+# Build images for Camel routes
+mvn clean install -Pdocker-build
+
+# Start containers with Camel routes
 docker run -d -p 18080:18080 --link amqbroker:amqbroker apachecon/apachecon-demo-router:1.0-SNAPSHOT
 docker run -d --link mongodb:mongodb --link amqbroker:amqbroker apachecon/apachecon-demo-router-processor:1.0-SNAPSHOT
