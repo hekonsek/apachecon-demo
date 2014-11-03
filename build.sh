0fe2f60d4ce72f27e4bb4c2edf071136a50a1228
Henryk Konsek <hekonsek@gmail.com>
2014-11-03 11:13:02 +0100

    Added MongoDB to build script.

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ mvn -f apachecon-demo-router/pom.xml install docker:build
 
 docker stop $(docker ps -q)
 docker rm amqbroker
+docker rm mongodb
 
+docker run -d -p 27017:27017 --name mongodb dockerfile/mongodb
 docker run -d -v /tmp/amqbroker:/var/activemq --name amqbroker fabric8/fabric8-mq:hk.1
 docker run -d -p 18080:18080 --link amqbroker:amqbroker apachecon/apachecon-demo-router:1.0-SNAPSHOT
