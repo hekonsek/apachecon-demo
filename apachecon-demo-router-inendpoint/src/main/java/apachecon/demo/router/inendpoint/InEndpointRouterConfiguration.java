14293994d396ac8e69b81b363854215029a20d1d
Henryk Konsek <hekonsek@gmail.com>
2015-01-14 21:22:00 +0100

    Upgraded Spring Boot.

--- apachecon-demo-router-inendpoint/src/main/java/apachecon/demo/router/inendpoint/InEndpointRouterConfiguration.java
+++ apachecon-demo-router-inendpoint/src/main/java/apachecon/demo/router/inendpoint/InEndpointRouterConfiguration.java
@@ -1,40 +1,25 @@
 package apachecon.demo.router.inendpoint;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jms.connection.CachingConnectionFactory;
-
-import javax.jms.ConnectionFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 import static apachecon.demo.router.inendpoint.RandomUUIDExpression.randomUUID;
 
-@EnableAutoConfiguration
-public class InEndpointRouterConfiguration {
+@SpringBootApplication
+@Component
+public class InEndpointRouterConfiguration extends RouteBuilder {
 
-    public static void main(String[] args) throws InterruptedException {
-        new SpringApplication(InEndpointRouterConfiguration.class).run(args);
+    public static void main(String[] args) {
+        new SpringApplication("apachecon.demo.router.inendpoint").run(args);
     }
 
-    @Bean
-    RoutesBuilder nettyEndpoint() {
-        return new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                from("netty-http:http://0.0.0.0:18080").
-                        setBody(randomUUID()).
-                        inOnly("jms:invoices");
-            }
-        };
-    }
-
-    @Bean
-    ConnectionFactory connectionFactory() {
-        return new CachingConnectionFactory(new ActiveMQConnectionFactory("tcp://amqbroker:6162"));
+    @Override
+    public void configure() throws Exception {
+        from("netty-http:http://0.0.0.0:18080").
+                setBody(randomUUID()).
+                inOnly("jms:invoices");
     }
 
 }
-
