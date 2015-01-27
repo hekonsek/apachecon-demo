4045af4731515e9f0264379e18fea9a6aaf80b95
Henryk Konsek <hekonsek@gmail.com>
2015-01-27 22:45:24 +0100

    Refactoring.

--- apachecon-demo-router-inendpoint/src/main/java/apachecon/demo/router/inendpoint/InEndpointRouterConfiguration.java
+++ apachecon-demo-router-inendpoint/src/main/java/apachecon/demo/router/inendpoint/InEndpointRouterConfiguration.java
@@ -1,19 +1,12 @@
 package apachecon.demo.router.inendpoint;
 
-import org.apache.camel.builder.RouteBuilder;
-import org.springframework.boot.SpringApplication;
+import org.apache.camel.spring.boot.FatJarRouter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 import static apachecon.demo.router.inendpoint.RandomUUIDExpression.randomUUID;
 
 @SpringBootApplication
-@Component
-public class InEndpointRouterConfiguration extends RouteBuilder {
-
-    public static void main(String[] args) {
-        new SpringApplication("apachecon.demo.router.inendpoint").run(args);
-    }
+public class InEndpointRouterConfiguration extends FatJarRouter {
 
     @Override
     public void configure() throws Exception {
