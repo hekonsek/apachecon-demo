1fe1d25126dcfec6866d29227bfac454ee17a7c6
Henryk Konsek <hekonsek@gmail.com>
2015-02-02 12:39:17 +0100

    Refactoring.

--- demo.sh
+++ demo.sh
@@ -10,7 +10,7 @@ docker run -d -p 27017:27017 --name mongodb dockerfile/mongodb
 docker run -d -v /tmp/amqbroker:/var/activemq --name amqbroker fabric8/fabric8-mq:hk.1
 
 # Build images for Camel routes
-mvn clean install -Pdocker-build
+mvn clean install
 
 # Start containers with Camel routes
 docker run -d -p 18080:18080 --link amqbroker:amqbroker apachecon/apachecon-demo-router-inendpoint:1.0-SNAPSHOT
